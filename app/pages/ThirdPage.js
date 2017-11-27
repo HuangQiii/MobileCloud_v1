bb24347e7321a9a3306304273f78fffef452d3d3
HuangQiii <598260403@qq.com>
2017-11-27 19:49:24 +0800

    [ADD] 添加回复

--- app/pages/ThirdPage.js
+++ app/pages/ThirdPage.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, Linking, NetInfo } from 'react-native';
+import { StyleSheet, Text, View, Image, Linking, NetInfo, TextInput } from 'react-native';
 import Button from '../components/Button';
 import Icon from 'react-native-vector-icons/Ionicons';
 
+let feedbackText;
 const aboutLogo = require('../images/about_logo.png');
 export default class ThirdPage extends Component {
 
@@ -13,7 +14,7 @@ export default class ThirdPage extends Component {
         ),
         headerRight: (
             <Icon.Button
-                name="logo-github"
+                name="md-checkmark"
                 backgroundColor="transparent"
                 underlayColor="transparent"
                 activeOpacity={0.8}
@@ -44,20 +45,23 @@ export default class ThirdPage extends Component {
                 connectionInfo: connectionInfo.type
             });
         });
+        feedbackText = '';
     }
 
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.content}>
+
                     <View style={styles.center}>
                         <Image style={styles.logo} source={aboutLogo} />
+                        <Text style={styles.title}>数据中心</Text>
                         <Text style={styles.version}>v1.0.0</Text>
-                        <Text style={styles.title}>MobileCloud</Text>
-                        <Text style={styles.subtitle}>假装是版权说明</Text>
-                        <Text>当前网络链接类型：{this.state.connectionInfo}</Text>
+                        <Text style={styles.subtitle}>QDLW Group</Text>
+                        {/*<Text>当前网络链接类型：{this.state.connectionInfo}</Text>*/}
                     </View>
-                    <View style={styles.bottomContainer}>
+
+                    {/*<View style={styles.bottomContainer}>
                         <View style={styles.disclaimerContent}>
                             <Text style={[styles.disclaimer, { color: '#999999' }]}>
                                 免责声明：
@@ -68,7 +72,23 @@ export default class ThirdPage extends Component {
                                 onPress={() => this.checkNetInfo()}
                             />
                         </View>
-                    </View>
+                    </View>*/}
+
+                    <TextInput
+                        ref={(ref) => {
+                            this.textInput = ref;
+                        }}
+                        style={styles.textInput}
+                        placeholder="如果你有什么想和我们交流，请写下！"
+                        placeholderTextColor="#aaaaaa"
+                        underlineColorAndroid="transparent"
+                        numberOfLines={20}
+                        multiline
+                        //autoFocus
+                        onChangeText={(text) => {
+                            feedbackText = text;
+                        }}
+                    />
                 </View>
             </View>
         );
@@ -93,7 +113,7 @@ const styles = StyleSheet.create({
     logo: {
         width: 110,
         height: 110,
-        marginTop: 50
+        marginTop: 30
     },
     version: {
         fontSize: 16,
@@ -108,9 +128,10 @@ const styles = StyleSheet.create({
         marginTop: 10
     },
     subtitle: {
-        fontSize: 16,
+        fontSize: 10,
         textAlign: 'center',
-        color: '#4e4e4e'
+        color: '#4e4e4e',
+        marginTop: 10
     },
     disclaimerContent: {
         flexDirection: 'column'
@@ -121,5 +142,12 @@ const styles = StyleSheet.create({
     },
     bottomContainer: {
         alignItems: 'center'
+    },
+    textInput: {
+        flex: 1,
+        fontSize: 14,
+        padding: 15,
+        backgroundColor: '#F2F2F2',
+        textAlignVertical: 'top'
     }
 });
